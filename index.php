865a2bee17417d0293e9c43668854837e0b243ca
AlejoGit <jaccortes@hotmail.com>
2021-10-15 14:41:46 -0500

    cambio

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!-- cabecera de pagina -->
+	<head>
+		<!-- titulo de la pagina -->
+		<title>Sombreros Vueltiaos - Mintic Grupo 5-3</title>
+		<meta charset="utf-8">
+		<!-- configuracion responsive -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- cargue de hoja de estilos -->
+		<link href="css\SombrerosVueltiaos.css" rel="stylesheet">
+		<!-- cargue del icono de la pagina -->
+		<link rel="icon" type="image/ico"href="img\icono.ico">
+	</head>
+	<!-- cuerpo de la pagina -->
+	<body>
+		<main>
+			<!-- imagen menu y titulo de la pagina -->
+			<header class="imagendelheader">
+				<nav>
+					<ul>
+						<li><a class="active" href="SombrerosVueltiaos">Home</a></li>
+						<li><a href="productos">Productos</a></li>
+						<li><a href="pedidos">Pedidos</a></li>
+						<li><a href="seguimiento">Seguimiento</a></li>
+					</ul>
+					<div >
+						<div class="hero-text">
+							<h1 class="h1deheader">Sombreros Vueltiaos</h1>
+							<p>Artesanales</p>
+						</div>
+					</div>
+				</nav>
+			</header>
+			<!-- seccion de servicios de la pagina -->
+			<section class="services-section" id="services">
+				<div class="cols">
+						<!-- servicio de mostrar productos-->
+						<div class="container">
+							<img src="img\productosseccion.jpg" alt="Avatar" class="image">
+							<div class="overlay">
+								<div class="texto">
+									<div class="hero-image">
+									  <div class="hero-text">
+										<h2 style="font-size:50px">PRODUCTOS</h2>
+										<p>Disfruta de Nuestros Productos</p>
+										<button onclick="window.location.href='productos'">VER</button>
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- servicio de pedidos en linea -->
+						<div class="container">
+							<img src="img\pedidosservicio.png" alt="Avatar" class="image">
+							<div class="overlay">
+								<div class="texto">
+									<div class="hero-image">
+									  <div class="hero-text">
+										<h2 style="font-size:50px">REALIZAR UN PEDIDO</h2>
+										<p>Viste de Colombia</p>
+										<button onclick="window.location.href='pedidos'">COMPRAR</button>
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>		
+						<!-- servicio de tracking de orden -->
+						<div class="container">
+							<img src="img\seguimientoseccion.jpg" alt="Avatar" class="image">
+							<div class="overlay">
+								<div class="texto">
+									<div class="hero-image">
+									  <div class="hero-text">
+										<h2 style="font-size:50px">ESTADO DEL PEDIDO</h2>
+										<p>Realiza Seguimiento a tu Pedido</p>
+										<button onclick="window.location.href='seguimiento'">SEGUIR</button>
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>
+				</div>
+			</section>
+			<!-- seccion de noticias -->
+			<section class="news-section" id="news">
+			<h2 class="h2notiequi">NOTICIAS</h2>
+				<div class="cols2">
+					<!-- noticia1 -->
+					<div class="containernoti">
+						<p class="textonoti">
+							<img class="imagennoticiader" src="img/edificiosombrero.jpg">
+							<span class="capitaletra">E</span>l parador turístico es cubierto por un enorme sombrero vueltiao que no es de paja sino de hierro y concreto y si las...	 <button onclick="window.location.href='noticia1'">VER</button>					
+						</p>
+					</div>
+					<div class="containernotisepara"></div>
+					<!-- noticia2 -->
+					<div class="containernoti">
+						<p class="textonoti">
+							<img class="imagennoticiaizq" src="img/haciendosombrero.jpg">
+							<span class="capitaletra">S</span>e elabora con la fibra de la caña flecha, de la cual se extrae la fibra vegetal con la que la comunidad indígena Zenú elabora Dif...		<button onclick="window.location.href='noticia2'">VER</button>						
+						</p>
+					</div>
+				</div>
+				<div class="cols3"></div>
+				<div class="cols2">
+					<!-- noticia3 -->
+					<div class="containernoti">
+						<p class="textonoti">
+							<img class="imagennoticiader" src="img/mujersombrero.jpg">
+							<span class="capitaletra">E</span>l sombrero vueltiao de Tuchín tiene imitación 'Made in China' Se venden más baratos, por las calles se consiguen...		<button onclick="window.location.href='noticia3'">VER</button>						
+						</p>
+					</div>
+					<div class="containernotisepara"></div>
+					<!-- noticia4 -->
+					<div class="containernoti">
+						<p class="textonoti">
+							<img class="imagennoticiaizq" src="img/papasombrero.jpg">
+							<span class="capitaletra">A</span>sí reaccionó el papa cuando le pusieron el sombrero vueltiao en Villavicencio, aunque no es típico de la región llanera...			<button onclick="window.location.href='noticia4'">VER</button>					
+						</p>
+					</div>
+				</div>		
+				<div class="cols">
+				</div>		
+			</section>
+			<!-- seccion de equipo de trabajo -->
+			<section class="team-section"  id="team">
+				<h2 class="h2notiequi">EQUIPO</h2>
+					<div class="cols">
+						<div class="container">
+							<img src="img\cortes.png" alt="Avatar" class="image">
+							<div class="overlay">
+								<div class="texto">
+									<div class="hero-image">
+									  <div class="hero-text">
+										<h2 style="font-size:50px">ALEJANDRO CORTES</h2>
+										<p>Corporacion Unificada Nacional - CUN</p><br>
+										Pasatiempo: Sacarle la piedra a la gente<br><br>
+										Edad: unos cuantos...<br>
+										Frase: Si hay más allá, hay más acá?
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="container">
+							<img src="img\barraza.png" alt="Avatar" class="image">
+							<div class="overlay">
+								<div class="texto">
+									<div class="hero-image">
+									  <div class="hero-text">
+										<h2 style="font-size:50px">KEINERS BARRAZA</h2>
+										<p>Universidad de La Guajira</p><br>
+										Pasatiempo: Jugar Xbox<br>
+										Edad: 22 años<br>
+										Frase: Lo esencial es invisible a los ojos
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>					
+						<div class="container">
+							<img src="img\luis.png" alt="Avatar" class="image">
+							<div class="overlay">
+								<div class="texto">
+									<div class="hero-image">
+									  <div class="hero-text">
+										<h2 style="font-size:50px">LUIS ANGEL</h2>
+										<p>Universidad Surcolombiana</p><br>
+										Pasatiempo: hacer deporte<br>
+										Edad: 21 años<br>
+										Frase: La mayor gloria no es nunca caerse, sino levantarse siempre
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- seccion de pie de pagina del sitio -->
+			<footer id="footer-section"><br><br>
+				<div class="container-cont">
+					<div class="contacto-m">
+						<p class="contacto">
+							CONTACTO
+						</p>
+						<p class="titulo-contacto">
+							ALEJANDRO CORTES<br>
+							Programador & Scrum Master
+						</p>
+					</div>
+					<div class="contacto-m">
+						<p class="contacto">
+							CONTACTO
+						</p>
+						<p class="titulo-contacto">
+							KEINERS BARRAZA<br>
+							Product BackLog & StakeHolder
+						</p>
+					</div>
+					<div class="contacto-m">
+						<p class="contacto">
+							CONTACTO
+						</p>
+						<p class="titulo-contacto">
+							LUIS BOCANEGRA <br>
+							Diseño & Historias de Usuario
+						</p>
+					</div>			
+				</div>
+				<div id="p-footer">
+					<div class="container-2">
+						<div class="copyright">
+							<p class="titulo-footer">
+								Sitio dedicado a la comercializacion de sombreros vueltiaos con servicio de 
+								peddos y envio a domicilio, motivado con el objetivo de conectar a fabricantes 
+								con compradores de esta bonita artesania nacional colombiana. Copyright © 2021 All 
+								rights reserved.
+								<a class="link" href="https://github.com/jaccortes/semana-1-3/" target="_blank"> Repositorio Grupal</a>
+							</p>
+						</div>
+					</div>
+				</div>	
+			</footer>
+		</main>
+	</body>
+</html>
